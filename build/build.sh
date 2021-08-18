c8065873c36e9e683da9862d8ed08b1d526ba7e7
nomadli <dzym79@qq.com>
2021-08-18 16:30:43 +0800

    try fixed keychain depends

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,8 @@ base_path=$(cd $(dirname $0); pwd)
 
 pushd ${base_path}
     dpkg-deb --build $1
+    if [[ -f "../debs/$1.deb" ]]; then
+        rm -rf ../debs/$1.deb
+    fi
+    mv $1.deb ../debs/
 popd
